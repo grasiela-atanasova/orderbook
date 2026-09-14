bf5b79bceb711378eba40ad8b238d72079511783
Grasiela Atanasova <grasielaatanasova@gmail.com>
2026-09-15 01:35:50 +0200

    Add Order and Trade structs

--- order.hpp
+++ order.hpp
@@ -10,3 +10,19 @@ enum class OrderType{
     Limit,
     Market
 };
+
+struct Order{
+    uint64_t id;
+    Side side;
+    OrderType type;
+    uint64_t price;
+    uint64_t quantity;
+    uint64_t sequence;
+};
+
+struct Trade{
+    uint64_t buy_order_id;
+    uint64_t sell_order_id;
+    uint64_t price;
+    uint64_t quantity;
+};
