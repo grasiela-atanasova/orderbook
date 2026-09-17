2a540cd1c5852eb94f0c206da52cd22d3f301dc8
Grasiela Atanasova <grasielaatanasova@gmail.com>
2026-09-17 11:29:22 +0200

    Add OrderLocation struct, index, and cancel declaration

--- orderbook.hpp
+++ orderbook.hpp
@@ -4,15 +4,25 @@
 #include <list>
 #include "order.hpp"
 #include <vector>
+#include <unordered_map>
 
-using PriceLevel = std::list<Order>; //the list of Orders per price
+using PriceLevel = std::list<Order>; //the list of Orders on each price level
+
+struct OrderLocation{
+    Side side;
+    uint64_t price;
+    PriceLevel::iterator it; //keeps where exactly each order is so we can cancel it at O(1)
+};
 
 class OrderBook{
 public:
     void add_limit_order(const Order& order);
     void print_book () const;
     std::vector <Trade> add_order (const Order& incoming);
+    bool cancel(uint64_t order_id);
+
 private: //encapsulation
     std::map<uint64_t, PriceLevel> asks_;
     std::map<uint64_t, PriceLevel, std::greater<uint64_t>> bids_; 
+    std::unordered_map<uint64_t, OrderLocation> index_;
 };
